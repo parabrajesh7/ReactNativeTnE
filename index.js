2411bda7cb2db5d325116cba869d5c8345564d4f
prasadkawalekar@gmail.com <prasad.kawalekar@expenzing.local>
2020-07-31 16:10:39 +0530

    Prasad

--- index.js
+++ index.js
@@ -7,20 +7,5 @@ import App from './App';
 import {name as appName} from './app.json'; 
 
 AppRegistry.registerComponent(appName, () => App);
-//removed spaces.
-//Test
 
-
-// end of the file.
-
-//prasad changes new trial 3rd
-
-//new change !!!!
-//prasad changes new
-
-//final test
-
-
-
-//End of code
-//qwerty
+//Fresh commit chnages
