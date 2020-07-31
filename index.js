b4cb7c5a8709aa2b9075a1a890b9f9c249efdaf6
prasadkawalekar@gmail.com <prasad.kawalekar@expenzing.local>
2020-07-31 16:18:07 +0530

    Prasad

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json'; 
 
-AppRegistry.registerComponent(appName, () => App);
-//Jai shri ram
-//removed spaces.
-//Test
+//Commenting here
 
-//Fresh commit chnages
+AppRegistry.registerComponent(appName, () => App);
+
